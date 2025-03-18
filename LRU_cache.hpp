5e4b593557445987efce32173b67d98668f805f6
John Doe <dany.gurchin@gmail.com>
2025-03-18 21:30:44 +0300

    1.1.0 version - LRU_cache added

--- LRU_cache.hpp
+++ LRU_cache.hpp
@@ -6,12 +6,25 @@
 #include <tuple>
 #include <list>
 
+#include <functional>
+namespace std {
+    template <class... Ts>
+    class hash<std::tuple<Ts...>> {
+    public:
+        std::size_t operator()(const std::tuple<Ts...>& _tuple) const {
+            return std::apply([](const Ts&... args) { 
+                return (... ^ std::hash<std::decay_t<Ts>>{}(args));
+            }, _tuple);
+        }
+    };
+}
+
 namespace gtd {
     template <class rT, class... argTs>
     class LRU_cache {
     public:
         explicit LRU_cache(rT(*_ptr)(argTs...)) : func_ptr{_ptr} {}
-        explicit LRU_cache(rT(*_ptr)(argTs...), size_t _size) : func_ptr{_ptr}, cache_size{_size} {}
+        explicit LRU_cache(rT(*_ptr)(argTs...), std::size_t _size) : func_ptr{_ptr}, cache_size{_size} {}
         rT operator()(argTs... args) {
             std::tuple<argTs...> tuple_args = std::make_tuple(args...);
             if(cache_map.count(tuple_args)) {
@@ -30,8 +43,8 @@ namespace gtd {
         }
     private:
         rT(*func_ptr)(argTs...){};
-        size_t cache_size{};
+        std::size_t cache_size{};
         std::list<std::pair<std::tuple<argTs...>,rT>> cache{};
-        std::unordered_map<std::tuple<argTs...>, typename std::list<std::pair<std::tuple<argTs...>,rT>>::iterator> cache_map{};
+        std::unordered_map<std::tuple<argTs...>, typename std::list<std::pair<std::tuple<argTs...>,rT>>::iterator, std::hash<std::tuple<argTs...>>> cache_map{};
     };
 }
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Decorator Library is my C++ library for creating various function decorators. It
 
 > Library is in development so this section is updated together with library itself
 
-### Library connection
+## Library connection
 
 ```cpp
 #include "DecoratorLibrary.hpp"
@@ -17,10 +17,46 @@ Decorator Library is my C++ library for creating various function decorators. It
 
 > This section is in development...
 
+## LRU_cache
+
+> This section is in development...
+
+<details>
+<summary>Code example</summary>
+
+> This section is in development...
+
+```cpp
+#include <iostream>
+#include "LRU_cache.hpp" // #include "DecoratorLibrary.hpp"
+#include <cmath>
+#include <thread>
+#include <chrono>
+
+unsigned long long int func(unsigned int n) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    return std::pow(n,n);
+}
+
+int main() {
+    gtd::LRU_cache<unsigned long long int, unsigned int> lru_func(func,2); // gtd::LRU_cache lru_func(func,2);
+    std::cout << lru_func(8u) << std::endl;
+    std::cout << lru_func(3u) << std::endl;
+    std::cout << lru_func(2u) << std::endl;
+    std::cout << lru_func(2u) << std::endl;
+    std::cout << lru_func(3u) << std::endl;
+    std::cout << lru_func(8u) << std::endl;
+    return 0;
+}
+```
+
+</details>
+
 # Versions
 
-**Current version: 2.1.1**
+**Current version: 1.1.0**
 
 + 1.x.x
   + 1.0.x
+    + 1.1.0 - LRU_cache added
     + 1.0.0 - base functional of Decorator base-class
