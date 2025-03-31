73c2d0c269ac505c1a9bfbda163d231d3cecfab4
John Doe <dany.gurchin@gmail.com>
2025-04-01 01:15:28 +0300

    New LRU_cache's methods

--- LRU_cache.hpp
+++ LRU_cache.hpp
@@ -22,28 +22,61 @@ namespace gtd {
     template <typename rT, typename... argTs>
     class LRU_cache {
     public:
-        explicit LRU_cache(rT(*_ptr)(argTs...)) : func_ptr{_ptr} {}
-        explicit LRU_cache(rT(*_ptr)(argTs...), std::size_t _size) : func_ptr{_ptr}, cache_size{_size} {}
+        explicit LRU_cache(rT(*_ptr)(argTs...)) : __func_ptr{_ptr} {}
+        explicit LRU_cache(rT(*_ptr)(argTs...), std::size_t _size) : __func_ptr{_ptr}, __cache_size{_size} {}
         rT operator()(argTs... args) {
             std::tuple<argTs...> tuple_args = std::make_tuple(args...);
-            if(cache_map.count(tuple_args)) {
-                cache.splice(cache.begin(),cache,cache_map[tuple_args]);
-                return cache_map[tuple_args]->second;
+            if(__cache_map.count(tuple_args)) {
+                __cache.splice(__cache.begin(),__cache,__cache_map[tuple_args]);
+                return __cache_map[tuple_args]->second;
             } else {
-                if(cache_size != 0ull) { if(cache_map.size() == cache_size) {
-                    cache_map.erase(cache.back().first);
-                    cache.pop_back();
+                if(__cache_size != 0ull) { if(__cache_map.size() == __cache_size) {
+                    __cache_map.erase(__cache.back().first);
+                    __cache.pop_back();
                 } }
-                rT _return = func_ptr(args...);
-                cache.push_front({tuple_args,_return});
-                cache_map[tuple_args] = cache.begin();
+                rT _return = __func_ptr(args...);
+                __cache.push_front({tuple_args,_return});
+                __cache_map[tuple_args] = __cache.begin();
                 return _return;
             }
         }
+        std::size_t cache_size() {
+            return __cache_map.size();
+        }
+        void change_cache_size(std::size_t new_size) {
+            if(new_size >= cache_size()) return;
+            for(unsigned long long int i{}; i < cache_size()-new_size; ++i) {
+                __cache_map.erase(__cache.back().first);
+                __cache.pop_back();
+            }
+        }
+        std::size_t cache_capacity() {
+            return __cache_size;
+        }
+        void change_cache_capacity(std::size_t new_size) {
+            if(__cache_size > new_size && new_size != 0) {
+                for(unsigned long long int i{}; i < __cache_size-new_size; ++i) {
+                    __cache_map.erase(__cache.back().first);
+                    __cache.pop_back();
+                }
+            }
+            __cache_size = new_size;
+        }
+        void clear_cache() {
+            __cache_map.clear();
+            __cache.clear();
+        }
+        void clear_cache(argTs... args) {
+            clear_cache(std::make_tuple(args...));
+        }
+        void clear_cache(std::tuple<argTs...> args) {
+            __cache.erase(__cache_map[args]);
+            __cache_map.erase(args);
+        }
     private:
-        rT(*func_ptr)(argTs...){};
-        std::size_t cache_size{};
-        std::list<std::pair<std::tuple<argTs...>,rT>> cache{};
-        std::unordered_map<std::tuple<argTs...>, typename std::list<std::pair<std::tuple<argTs...>,rT>>::iterator> cache_map{};
+        rT(*__func_ptr)(argTs...){};
+        std::size_t __cache_size{};
+        std::list<std::pair<std::tuple<argTs...>,rT>> __cache{};
+        std::unordered_map<std::tuple<argTs...>, typename std::list<std::pair<std::tuple<argTs...>,rT>>::iterator> __cache_map{};
     };
 }
